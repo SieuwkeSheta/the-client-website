a48916b38afde772f13a1b04004f43c071eb754e
SieuwkeSheta <sieuwke_sheta@hotmail.nl>
2025-10-09 12:38:25 +0200

    Alle eerste elementen CSS en JS #5
    - JS Hamburger menu en carousel knoppen
    - CSS eerste elementen

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,38 @@
-// JS
+// hamburger
+const hamMenu = document.querySelector(".ham-menu")
+const hamMenuButton = document.querySelector(".ham-menu-button")
+const greyScale = document.querySelector(".grey-scale")
+
+hamMenuButton.addEventListener("click", toggleHamMenu)
+greyScale.addEventListener("click", toggleHamMenu)
+
+function toggleHamMenu() {
+    hamMenu.classList.toggle("is-open")
+    greyScale.classList.toggle("is-open")
+}
+
+
+
+// Laatste nieuws op home-pagina, links en rechts knoppen op carousel
+const prevBtn = document.querySelector(".prev1");
+const nextBtn = document.querySelector(".next1");
+const deCaro = document.querySelector(".carousel");
+const eenSlide = document.querySelector(".slide");
+
+prevBtn.addEventListener("click", toPrevSlide);
+nextBtn.addEventListener("click", toNextSlide);
+
+function toPrevSlide() {
+    let slideWidth = eenSlide.offsetWidth;
+
+    deCaro.scrollLeft = deCaro.scrollLeft - slideWidth;
+}
+
+function toNextSlide() {
+    let slideWidth = eenSlide.offsetWidth;
+
+    deCaro.scrollLeft = deCaro.scrollLeft + slideWidth;
+}
+
+
+
